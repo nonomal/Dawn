f9403ada79bab75d45af5cfecd5df7bc20231112
Harsh Shandilya <me@msfjarvis.dev>
2021-02-01 18:49:42 +0530

    Limit Glide cache to 200 MB (#381)

--- app/src/main/java/me/saket/dank/utils/glide/DankAppGlideModule.java
+++ app/src/main/java/me/saket/dank/utils/glide/DankAppGlideModule.java
@@ -7,7 +7,10 @@ import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.annotation.Excludes;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpLibraryGlideModule;
+import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory;
 import com.bumptech.glide.module.AppGlideModule;
+
+import java.io.File;
 
 /**
  * Glide requires atleast one app module if library modules are used.
@@ -16,6 +19,12 @@ import com.bumptech.glide.module.AppGlideModule;
 @Excludes({ OkHttpLibraryGlideModule.class })
 public class DankAppGlideModule extends AppGlideModule {
 
+  // Must be kept in sync with file_provider_paths.xml
+  private static final String GLIDE_DISK_CACHE_FOLDER_NAME = "image_manager_disk_cache";
+
+  // 200 MB in bytes as expected by Glide
+  private static final long GLIDE_DISK_CACHE_SIZE = 200 * 1024 * 1024;
+
   @Override
   public boolean isManifestParsingEnabled() {
     return false;
@@ -23,6 +32,8 @@ public class DankAppGlideModule extends AppGlideModule {
 
   @Override
   public void applyOptions(Context context, GlideBuilder builder) {
+    final String cacheDirectoryPath = new File(context.getCacheDir(), GLIDE_DISK_CACHE_FOLDER_NAME).getAbsolutePath();
     builder.setLogLevel(Log.ERROR);
+    builder.setDiskCache(() -> new DiskLruCacheFactory(cacheDirectoryPath, GLIDE_DISK_CACHE_SIZE).build());
   }
 }
